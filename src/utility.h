e12906cd0e21d40d0c31ee093b912a2ae10b950e
pixlark <t1000.pixlark@gmail.com>
2017-12-21 04:54:10 -0500

    dynamic array in utils

--- src/utility.h
+++ src/utility.h
@@ -29,6 +29,94 @@ struct Color {
 	Color(unsigned char r, unsigned char g, unsigned char b) { this->r = r; this->g = g; this->b = b; }
 };
 
+template <typename T>
+struct List {
+	T * arr;
+	int len;
+	int len_max;
+	void Alloc();
+	void Push(T p);
+	T Pop();
+	T& operator[](int indice);
+	int Remove(int indice);
+	
+	void _Grow();
+	void _Shrink();
+};
+
+template <typename T>
+void List<T>::Alloc()
+{
+	len = 0;
+	len_max = 4;
+	arr = (T*) malloc(sizeof(T) * len_max);
+}
+
+template <typename T>
+void List<T>::Push(T p)
+{
+	if (len >= len_max) {
+		_Grow();
+	}
+	arr[len++] = p;
+}
+
+template <typename T>
+T List<T>::Pop()
+{
+	assert(len > 0);
+	T ret = arr[--len];
+	if (len <= len_max / 4 && len_max > 4) {
+		_Shrink();
+	}
+	return ret;
+}
+
+template <typename T>
+T& List<T>::operator[](int indice)
+{
+	return arr[indice];
+}
+
+template <typename T>
+int List<T>::Remove(int indice)
+{
+	assert(indice < len);
+	assert(len > 0);	
+	for (int i = indice; i < len - 1; i++) {
+		arr[i] = arr[i + 1];
+	}
+	len--;
+	if (len <= len_max / 4 && len_max > 4) {
+		_Shrink();
+	}
+	return 0;
+}
+
+template <typename T>
+void List<T>::_Grow()
+{
+	T * new_arr = (T*) malloc(sizeof(T) * len_max * 2);
+	for (int i = 0; i < len_max; i++) {
+		new_arr[i] = arr[i];
+	}
+	free(arr);
+	len_max *= 2;
+	arr = new_arr;
+}
+
+template <typename T>
+void List<T>::_Shrink()
+{
+	T * new_arr = (T*) malloc(sizeof(T) * (len_max / 2));
+	for (int i = 0; i < (len_max / 2); i++) {
+		new_arr[i] = arr[i];
+	}
+	free(arr);
+	len_max /= 2;
+	arr = new_arr;
+}
+
 void os_path(char * path, int len);
 char * find_path(char * name, char * directory);
 
